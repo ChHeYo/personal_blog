81033b3d8456606ce49cbff5ed5215d6a2cfb8ca
ChHeYo <cyoo3@u.rochester.edu>
2017-10-29 00:15:10 -0400

    changed production

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -1,14 +1,3 @@
 from .base import *
 
-DEBUG = True
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'personal_blog_db',
-        'USER': 'jason_blog',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+DEBUG = False
